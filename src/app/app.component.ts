b2ab636e99f7c985ed3632fe165f4e7ec40f9ac2
Cristian <cristianpand@yahoo.com>
2018-10-08 13:18:31 +0200

    refactored check extension method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public isExtensionInstalled() {
+    // The ID of the extension we want to talk to.
+    const editorExtensionId = 'odijigegicopmlagdknbdeelggiocicj';
+
+    // Make a simple request:
+    chrome.runtime.sendMessage(editorExtensionId, { isRecordingExtensionInstalled: false }, function (response) {
+      if (!response) {
+        console.log('no response');
+        return false;
+      }
+      if (!!response.isRecordingExtensionInstalled && response.isRecordingExtensionInstalled === true) {
+        console.log('extension is installed :)');
+        return true;
+      } else {
+        console.log('extension is not installed');
+        return false;
+      }
+    });
+  }
 }
